c2db9c071aa6ea7445865a333fa529548803a82d
Pranay Gupta <pranayguptamain@gmail.com>
2021-11-16 12:41:43 +0530

    chore: formatted the code

--- Day-05/index.js
+++ Day-05/index.js
@@ -2,9 +2,8 @@ const str = 'XeroX';
 
 function getTheGapX(str) {
     if( str.lastIndexOf('X')==-1&& str.indexOf('X')==-1)
-    return -1;
+        return -1;
     else
-return str.lastIndexOf('X') - str.indexOf('X');
-
+        return str.lastIndexOf('X') - str.indexOf('X');
 }
 console.log(`Gap between the X's: ${getTheGapX(str)}`)
