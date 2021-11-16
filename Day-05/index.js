7fec13652d97d392fb9f12a29eb942a1b68961f4
shreya40 <88774355+shreya40@users.noreply.github.com>
2021-11-16 12:27:41 +0530

    Create index.js

---
+++ Day-05/index.js
@@ -0,0 +1,10 @@
+const str = 'XeroX';
+
+function getTheGapX(str) {
+    if( str.lastIndexOf('X')==-1&& str.indexOf('X')==-1)
+    return -1;
+    else
+return str.lastIndexOf('X') - str.indexOf('X');
+
+}
+console.log(`Gap between the X's: ${getTheGapX(str)}`)
