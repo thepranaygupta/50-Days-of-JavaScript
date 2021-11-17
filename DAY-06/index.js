8af34791ce3a6e5b7d9fb439cd294db764e3201a
Pranay <pranayguptamain@gmail.com>
2021-11-17 22:31:02 +0530

    feat: Add Day-06 solution

--- DAY-06/index.js
+++ DAY-06/index.js
@@ -2,16 +2,14 @@ const str = 'JavaScript is simple but not easy to master';
 const wordLimit = 3
 
 function truncateWithWordLimit(str, wordLimit) {
-    str=" "+str; let k=0; let s="";
-    for(let i=0;i<str.length;i++)
-    {
-        if(str.charAt(i)==" ")
-        k++;
-        if(k>wordLimit)
-        break;
-        s=s+str.charAt(i);
+    str = " " + str; let k = 0; let s = "";
+    for (let i = 0; i < str.length; i++) {
+        if (str.charAt(i) == " ")
+            k++;
+        if (k > wordLimit)
+            break;
+        s = s + str.charAt(i);
     }
-
     return s.trim();
 }
 
