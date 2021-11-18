a031b591c586b56d30d7a824c6f778be550c203f
Pranay Gupta <pranayguptamain@gmail.com>
2021-11-18 20:13:54 +0530

    feat: Add Day-07 solution

--- Day-07/index.js
+++ Day-07/index.js
@@ -2,9 +2,10 @@ const number = '+919876543210';
 
 function validateMobile(number) {
     let s=number+"";
-if(s.substring(0,3)==="+91"||s.charAt(0)==="0"||s.length==10)
-   return true;
-    else return false;
+    if(s.substring(0,3)==="+91"||s.charAt(0)==="0"||s.length==10)
+        return true;
+    else
+        return false;
 }
 
 console.log(`is a valid Indian mobile number: ${validateMobile(number)}`)
